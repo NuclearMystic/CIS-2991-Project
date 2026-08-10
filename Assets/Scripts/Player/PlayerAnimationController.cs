6024c84a187187ac2bbd3219e4ce2daca16b5f57
Andrew Gutholm <155109727+NuclearMystic@users.noreply.github.com>
2026-08-09 20:26:43 -0400

    feat/weapon sprite

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace CIS2991Project.Player
@@ -5,10 +7,30 @@ namespace CIS2991Project.Player
     [RequireComponent(typeof(Animator))]
     public sealed class PlayerAnimationController : MonoBehaviour
     {
+        [Serializable]
+        private struct DirectionalClips
+        {
+            public AnimationClip down;
+            public AnimationClip up;
+            public AnimationClip left;
+            public AnimationClip right;
+        }
+
         private enum FacingDirection { Down, Up, Left, Right }
 
+        [Header("Must match the clips already wired into the base controller's Idle states")]
+        [SerializeField] private DirectionalClips unarmedIdle;
+        [SerializeField] private DirectionalClips armedIdle;
+
+        [Header("Must match the clips already wired into the base controller's Run states")]
+        [SerializeField] private DirectionalClips unarmedRun;
+        [SerializeField] private DirectionalClips armedRun;
+
         private Animator _animator;
+        private AnimatorOverrideController _overrideController;
         private PlayerHealth _health;
+        private PlayerInventory _inventory;
+        private readonly List<KeyValuePair<AnimationClip, AnimationClip>> _overrideBuffer = new();
         private FacingDirection _facing = FacingDirection.Down;
         private bool _isDead;
 
@@ -16,16 +38,24 @@ namespace CIS2991Project.Player
         {
             _animator = GetComponent<Animator>();
             _health = GetComponent<PlayerHealth>();
+            _inventory = GetComponent<PlayerInventory>();
+
+            _overrideController = _animator.runtimeAnimatorController as AnimatorOverrideController;
+            if (_overrideController == null)
+                Debug.LogWarning("PlayerAnimationController: Animator's Controller must be an AnimatorOverrideController for equip/unequip clip swapping to work.");
         }
 
         private void OnEnable()
         {
             if (_health != null) _health.HealthChanged += HandleHealthChanged;
+            if (_inventory != null) _inventory.EquipmentChanged += HandleEquipmentChanged;
+            HandleEquipmentChanged();
         }
 
         private void OnDisable()
         {
             if (_health != null) _health.HealthChanged -= HandleHealthChanged;
+            if (_inventory != null) _inventory.EquipmentChanged -= HandleEquipmentChanged;
         }
 
         private void Update()
@@ -48,6 +78,31 @@ namespace CIS2991Project.Player
             _isDead = true;
             _animator.SetBool("IsDead", true);
             _animator.SetTrigger("Died");
+        }
+
+        private void HandleEquipmentChanged()
+        {
+            ApplyWeaponClips(_inventory != null && _inventory.EquippedWeapon != null);
+        }
+
+        private void ApplyWeaponClips(bool isArmed)
+        {
+            if (_overrideController == null)
+                return;
+
+            var idle = isArmed ? armedIdle : unarmedIdle;
+            var run = isArmed ? armedRun : unarmedRun;
+
+            _overrideBuffer.Clear();
+            _overrideBuffer.Add(new KeyValuePair<AnimationClip, AnimationClip>(unarmedIdle.down, idle.down));
+            _overrideBuffer.Add(new KeyValuePair<AnimationClip, AnimationClip>(unarmedIdle.up, idle.up));
+            _overrideBuffer.Add(new KeyValuePair<AnimationClip, AnimationClip>(unarmedIdle.left, idle.left));
+            _overrideBuffer.Add(new KeyValuePair<AnimationClip, AnimationClip>(unarmedIdle.right, idle.right));
+            _overrideBuffer.Add(new KeyValuePair<AnimationClip, AnimationClip>(unarmedRun.down, run.down));
+            _overrideBuffer.Add(new KeyValuePair<AnimationClip, AnimationClip>(unarmedRun.up, run.up));
+            _overrideBuffer.Add(new KeyValuePair<AnimationClip, AnimationClip>(unarmedRun.left, run.left));
+            _overrideBuffer.Add(new KeyValuePair<AnimationClip, AnimationClip>(unarmedRun.right, run.right));
+            _overrideController.ApplyOverrides(_overrideBuffer);
         }
 
         private static FacingDirection DetermineFacing(Vector2 input)
--- Assets/Scripts/Player/PlayerWeaponVisual.cs
+++ Assets/Scripts/Player/PlayerWeaponVisual.cs
@@ -5,15 +5,29 @@ namespace CIS2991Project.Player
     [RequireComponent(typeof(SpriteRenderer))]
     public sealed class PlayerWeaponVisual : MonoBehaviour
     {
+        [Header("Position offset per direction (added to this object's starting local position)")]
+        [SerializeField] private Vector2 downOffset;
+        [SerializeField] private Vector2 upOffset;
+        [SerializeField] private Vector2 leftOffset;
+        [SerializeField] private Vector2 rightOffset;
+
+        [Header("Sorting order per direction")]
+        [SerializeField] private int downSortingOrder = 3;
+        [SerializeField] private int upSortingOrder = 1;
+        [SerializeField] private int leftSortingOrder = 3;
+        [SerializeField] private int rightSortingOrder = 3;
+
         private Animator _animator;
         private PlayerInventory _inventory;
         private SpriteRenderer _renderer;
+        private Vector3 _basePosition;
 
         private void Awake()
         {
             _animator = GetComponentInParent<Animator>();
             _inventory = GetComponentInParent<PlayerInventory>();
             _renderer = GetComponent<SpriteRenderer>();
+            _basePosition = transform.localPosition;
         }
 
         private void LateUpdate()
@@ -25,8 +39,11 @@ namespace CIS2991Project.Player
                 return;
             }
 
-            _renderer.sprite = GetDirectionalSprite(weapon, _animator.GetInteger("Direction"));
+            var direction = _animator.GetInteger("Direction");
+            _renderer.sprite = GetDirectionalSprite(weapon, direction);
             _renderer.enabled = _renderer.sprite != null;
+            _renderer.sortingOrder = GetSortingOrder(direction);
+            transform.localPosition = _basePosition + (Vector3)GetOffset(direction);
         }
 
         private static Sprite GetDirectionalSprite(global::Item weapon, int direction)
@@ -37,6 +54,28 @@ namespace CIS2991Project.Player
                 2 => weapon.equippedSpriteLeft,
                 3 => weapon.equippedSpriteRight,
                 _ => weapon.equippedSpriteDown,
+            };
+        }
+
+        private Vector2 GetOffset(int direction)
+        {
+            return direction switch
+            {
+                1 => upOffset,
+                2 => leftOffset,
+                3 => rightOffset,
+                _ => downOffset,
+            };
+        }
+
+        private int GetSortingOrder(int direction)
+        {
+            return direction switch
+            {
+                1 => upSortingOrder,
+                2 => leftSortingOrder,
+                3 => rightSortingOrder,
+                _ => downSortingOrder,
             };
         }
     }
